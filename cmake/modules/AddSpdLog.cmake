522a8bbd812755910bb635265b67d2fe65c27295
Denys Asauliak <denoming@gmail.com>
2024-09-10 22:03:25 +0300

    feat: specify spdlog log level

--- cmake/modules/AddSpdLog.cmake
+++ cmake/modules/AddSpdLog.cmake
@@ -4,3 +4,23 @@
 # Denys Asauliak, denoming@gmail.com
 
 find_package(spdlog REQUIRED)
+
+if (VOXER_ACTIVE_LOG_LEVEL STREQUAL "None")
+    message(VERBOSE "Set spdlog log level to 'None'")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_OFF)
+elseif(VOXER_ACTIVE_LOG_LEVEL STREQUAL "Debug")
+    message(VERBOSE "Set spdlog log level to 'Debug'")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_DEBUG)
+elseif(VOXER_ACTIVE_LOG_LEVEL STREQUAL "Info")
+    message(VERBOSE "Set spdlog log level to 'Info'")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_INFO)
+elseif(VOXER_ACTIVE_LOG_LEVEL STREQUAL "Warning")
+    message(VERBOSE "Set spdlog log level to 'Warn'")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_WARN)
+elseif(VOXER_ACTIVE_LOG_LEVEL STREQUAL "Error")
+    message(VERBOSE "Set spdlog log level to 'Error'")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_ERROR)
+else()
+    message(VERBOSE "Set spdlog log level to 'Info' (by default)")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_INFO)
+endif()
