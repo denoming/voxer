9551923e9c96a058a268cb50684c87a5e7d0d215
Denys Asauliak <denoming@gmail.com>
2024-11-15 21:07:03 +0200

    feat: change default install prefix

--- cmake/ProjectInstall.cmake
+++ cmake/ProjectInstall.cmake
@@ -6,7 +6,7 @@
 if (NOT WIN32 AND CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
     # Set default install prefix path
     if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        set(CMAKE_INSTALL_PREFIX "/opt/denoming.com/${PROJECT_NAME}"
+        set(CMAKE_INSTALL_PREFIX "/usr"
             CACHE PATH "Default install prefix" FORCE)
     endif()
 endif()
